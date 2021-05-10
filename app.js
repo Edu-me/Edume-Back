57da5a27bc71da4fa45e40015a3ccd2e3692f84b
MohamedSamiMoahmed <mohamed.samy9956@gmail.com>
2021-05-10 03:05:08 +0200

    using cors

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const morgan = require('morgan')
 const express = require('express');
 serverDebugger = require('debug')('server')
 const app = express();
+let cors = require('cors')
 const addAdmin = require('./utils/adminRegister')
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
-
+app.use(cors())
 require('./start-up/routes')(app);
 require('./start-up/db')();
 const port = process.env.PORT || 3000;
